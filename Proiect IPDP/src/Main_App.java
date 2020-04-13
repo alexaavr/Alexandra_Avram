ab86dead716abd9e824adc2c398658579e474544
alexaavr <45763511+alexaavr@users.noreply.github.com>
2020-04-13 23:04:43 +0300

    Inventory
    Co-Authored-By: Grecu Georgian <ggrecu24@users.noreply.github.com>

--- Proiect IPDP/src/Main_App.java
+++ Proiect IPDP/src/Main_App.java
@@ -1,5 +1,3 @@
-import com.mongodb.BasicDBObject;
-import com.mongodb.DBObject;
 import com.mongodb.MongoClient;
 import com.mongodb.MongoClientURI;
 import com.mongodb.client.MongoCollection;
@@ -8,7 +6,11 @@ import javafx.application.Application;
 import javafx.geometry.Insets;
 import javafx.geometry.Pos;
 import javafx.scene.Scene;
-import javafx.scene.control.*;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+import javafx.scene.image.Image;
 import javafx.scene.layout.*;
 import javafx.scene.paint.Color;
 import javafx.stage.Stage;
@@ -18,6 +20,7 @@ import org.bson.Document;
 public class Main_App extends Application {
 
     Stage window;
+    User u ;
     Document d;
     String uri = "mongodb+srv://Alexa:alexa@mangodb-wcom9.mongodb.net/Login";
     MongoClientURI clientURI = new MongoClientURI(uri);
@@ -31,8 +34,8 @@ public class Main_App extends Application {
     }
 ////////////////////////                     DATABASE                        //////////////////////////////////////////////////////////////////////////////
 
-    private static final DBObject toDBObject(User u) {
-        return new BasicDBObject("First Name", u.getFirstName())
+    private static final Document toDocument(User u) {
+        return new Document("First Name", u.getFirstName())
                 .append("Last Name", u.getLastName())
                 .append("Age", u.getAge())
                 .append("Username", u.username)
@@ -44,11 +47,13 @@ public class Main_App extends Application {
     ////////////////////////                     INTERFACE                        //////////////////////////////////////////////////////////////////////////////
     @Override
     public void start(Stage primaryStage) throws Exception {
-
+//////////////////////////////////////////////                         WINDOW                              ////////////////////////////////////////////////////////
         window = primaryStage;
         window.setTitle("Login");
         window.setMinWidth(600);
         //window.setMaximized(true);
+        Image image = new Image("/icon/images.png");
+        window.getIcons().add(image);
 
 //////////////////////////////////////////////                         LOGIN SCENE                              ////////////////////////////////////////////////////////
 
@@ -58,9 +63,18 @@ public class Main_App extends Application {
         grid.setVgap(8);
         grid.setHgap(10);
         grid.setBackground(new Background(new BackgroundFill(Color.BLANCHEDALMOND, CornerRadii.EMPTY, Insets.EMPTY)));
+        grid.setAlignment(Pos.CENTER);
+
+
+        GridPane grid_REGISTER = new GridPane();/////////////////////////////////////////////////////////////               GRID REGISTER
+        grid_REGISTER.setPadding(new Insets(10, 10, 10, 10));
+        grid_REGISTER.setVgap(8);
+        grid_REGISTER.setHgap(10);
+        grid_REGISTER.setBackground(new Background(new BackgroundFill(Color.AQUAMARINE, CornerRadii.EMPTY, Insets.EMPTY)));
+        grid_REGISTER.setAlignment(Pos.CENTER);
 
         // scene
-        Scene scene_LOGIN = new Scene(grid, 300, 200);
+        Scene scene_LOGIN = new Scene(grid, 600, 300);
 
         //Name Label - constrains use (child, column, row)
         Label nameLabel = new Label("Username:");
@@ -76,11 +90,8 @@ public class Main_App extends Application {
 
         //Password Input
         PasswordField passInput = new PasswordField();
-        TextField pass_visible = new TextField();
         passInput.setPromptText("password");
         GridPane.setConstraints(passInput, 1, 1);
-        CheckBox box_show = new CheckBox("Show");
-        GridPane.setConstraints(box_show, 2, 1);
 
         //Login
         HBox hor = new HBox();
@@ -108,13 +119,15 @@ public class Main_App extends Application {
         GridPane.setConstraints(hor, 1, 2);
 
         //register
+        Scene scene_REGISTER = new Scene(grid_REGISTER, 600, 300);///////////////////////////////////////////////////////////////////////////  SCENE REGISTER INIT
         Button registerButton = new Button("Register (only users)");
+        registerButton.setOnAction(e -> window.setScene(scene_REGISTER));
         GridPane.setConstraints(registerButton, 0, 2);
 
 
         //Add everything to grid
-        grid.setAlignment(Pos.CENTER);
-        grid.getChildren().addAll(nameLabel, usernameInput, passLabel, passInput, hor, registerButton,box_show);
+
+        grid.getChildren().addAll(nameLabel, usernameInput, passLabel, passInput, hor, registerButton);
 
         window.setOnCloseRequest(e -> {
             e.consume();
@@ -124,6 +137,96 @@ public class Main_App extends Application {
         window.setScene(scene_LOGIN);
 
 ///////////////////////////////////////////////                         REGISTER SCENE                              ////////////////////////////////////////////////////////
+
+
+        //FN label
+        Label firstNameL_REGISTER = new Label("First name:");
+        GridPane.setConstraints(firstNameL_REGISTER, 0, 0);
+
+        //LN Label
+        Label lastNameL_REGISTER = new Label("Last name:");
+        GridPane.setConstraints(lastNameL_REGISTER, 0, 1);
+
+        //Name Label - constrains use (child, column, row)
+        Label usernameL_REGISTER = new Label("Username:");
+        GridPane.setConstraints(usernameL_REGISTER, 0, 2);
+
+        //Age label
+        Label ageL_REGISTER = new Label("Age:");
+        GridPane.setConstraints(ageL_REGISTER, 0, 5);
+
+        //Pass Label
+        Label passwordL_REGISTER = new Label("Password:");
+        GridPane.setConstraints(passwordL_REGISTER, 0, 3);
+
+        //E_mail label
+        Label mailL_REGISTER = new Label("E-mail adress:");
+        GridPane.setConstraints(mailL_REGISTER, 0, 4);
+
+
+
+        //FN text
+        TextField firstNameInput_REGISTER = new TextField("First name");
+        GridPane.setConstraints(firstNameInput_REGISTER, 1, 0);
+
+        //LN text
+        TextField lastNameInput_REGISTER = new TextField("Last name");
+        GridPane.setConstraints(lastNameInput_REGISTER, 1, 1);
+
+        //UserName Input
+        TextField usernameInput_REGISTER = new TextField("Username");
+        GridPane.setConstraints(usernameInput_REGISTER, 1, 2);
+
+        //Password Input
+        PasswordField passInput_REGISTER = new PasswordField();
+        passInput_REGISTER.setPromptText("Password");
+        GridPane.setConstraints(passInput_REGISTER, 1, 3);
+
+        //E-mail Input
+        TextField mailInput_REGISTER = new TextField("E-mail adress");
+        GridPane.setConstraints(mailInput_REGISTER, 1, 4);
+
+        //age input
+        TextField ageInput_REGISTER = new TextField("Age");
+        GridPane.setConstraints(ageInput_REGISTER, 1, 5);
+
+        /////buttons
+        HBox hor_REGISTER = new HBox();
+        hor_REGISTER.setSpacing(10);
+        hor_REGISTER.setAlignment(Pos.CENTER);
+
+        Button createAccount = new Button("Create account");
+        createAccount.setOnAction(e ->{
+            u = new User();
+            u.setFirstName(firstNameInput_REGISTER.getText().trim());
+            System.out.println(u.getFirstName());
+            u.setLastName(lastNameInput_REGISTER.getText().trim());
+            try{
+            u.setAge((Integer.parseInt(ageInput_REGISTER.getText().trim())));}
+            catch(NumberFormatException ex){
+                AlertBox.display("Alert", "Error: " + ageInput_REGISTER.getText().trim().toUpperCase() + " is not a number");
+            }
+            u.username = usernameInput_REGISTER.getText().trim();
+            u.password = passInput_REGISTER.getText().trim();
+            u.setMail_adress(mailInput_REGISTER.getText().trim());
+            System.out.println(u);
+            collection.insertOne(Main_App.toDocument(u));
+            AlertBox.display("Alerta", "IQ DIVIN CE ESTI !!!!!");////////////////////////////////////////////////////////////to main app
+        });
+
+        Button back_REGISTER = new Button("Back");
+        back_REGISTER.setOnAction(e -> window.setScene(scene_LOGIN));
+
+        hor_REGISTER.getChildren().addAll(createAccount, back_REGISTER);
+        GridPane.setConstraints(hor_REGISTER, 1, 6);
+
+        //add grid
+        grid_REGISTER.getChildren().addAll(firstNameL_REGISTER, firstNameInput_REGISTER, lastNameL_REGISTER, lastNameInput_REGISTER,
+                usernameL_REGISTER,usernameInput_REGISTER, passwordL_REGISTER, passInput_REGISTER, mailL_REGISTER, mailInput_REGISTER, ageL_REGISTER, ageInput_REGISTER,
+                hor_REGISTER);
+
+
+
 
         window.show();
     }
